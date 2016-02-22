9e88454df5bfed72ca2c9a33ffcb98ec46a13b7d
Yuliana Sarria García <Yuliana Sarria Garca>
2016-02-22 17:16:32 -0500

    Esto es un mensaje de prueba

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,6 @@
 using namespace std;
 
 int main() {
-    cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+    cout << "!!!Hola Mundo - Yuliana!!!" << endl; // prints !!!Hello World!!!
     return 0;
 }
